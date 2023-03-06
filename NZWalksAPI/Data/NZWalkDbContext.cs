0ac23735be32f170b0428fb59325724ac994351a
Deepthi <deepikafriends174@gmail.com>
2023-03-06 11:25:26 +0530

    Initial setup v0.0

---
+++ NZWalksAPI/Data/NZWalkDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalksAPI.Models.Domain;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalkDbContext: DbContext
+    {
+        public NZWalkDbContext(DbContextOptions<NZWalkDbContext> options) : base(options) { }   
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
